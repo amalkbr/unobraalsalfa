7c206a766c56fbce1187591fcb6e3ddb2ee88e42
Ahmedsami2hvuv <reozaki.1211@gmail.com>
2026-05-24 12:22:25 +0300

    Update index.py

--- api/index.py
+++ api/index.py
@@ -2330,10 +2330,10 @@ HTML_TEMPLATE = """
         function logout() { localStorage.clear(); location.href = "/"; }
 
         const sounds = {
-            click: new Audio('https://www.soundjay.com/buttons/sounds/button-16.mp3'),
-            reveal: new Audio('https://www.soundjay.com/buttons/sounds/button-3.mp3'),
-            win: new Audio('https://www.soundjay.com/misc/sounds/bell-ringing-05.mp3'),
-            fail: new Audio('https://www.soundjay.com/misc/sounds/fail-trombone-01.mp3')
+            click: new Audio('https://raw.githubusercontent.com/rafael-m-silva/Fast-Finger-Game/master/sounds/click.mp3'),
+            reveal: new Audio('https://raw.githubusercontent.com/rafael-m-silva/Fast-Finger-Game/master/sounds/level-up.mp3'),
+            win: new Audio('https://raw.githubusercontent.com/rafael-m-silva/Fast-Finger-Game/master/sounds/success.mp3'),
+            fail: new Audio('https://raw.githubusercontent.com/rafael-m-silva/Fast-Finger-Game/master/sounds/wrong.mp3')
         };
         function playSound(name) { sounds[name].currentTime = 0; sounds[name].play().catch(()=>null); }
 
